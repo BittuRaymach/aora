c22db76f0181ba041afd0bb9fd3b9bda01a2401d
BittuRaymach <bittu.kumar@raymach.in>
2024-07-31 15:45:04 +0530

    set up folder structure

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -25,7 +25,12 @@ export default function RootLayout() {
 
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
     </Stack>
   );
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,22 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View>
-      <Text className="text-3xl">Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center ">
+      <Text className="text-4xl font-poppinsBlack">Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href="/home">
+        <Text
+          style={{
+            color: "blue",
+            fontSize: 19,
+          }}
+        >
+          Go to home
+        </Text>
+      </Link>
     </View>
   );
 }
